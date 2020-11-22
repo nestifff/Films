03fd08714d85334d82e8eacbf2e04a6b3eee025e
nestifff <nestiv.01.12@gmail.com>
2020-11-23 02:12:23 +0300

    with movie details activity (static) 1

---
+++ app/src/main/java/com/example/films/MovieDetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.example.films
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) { }
+
+    }
+}
