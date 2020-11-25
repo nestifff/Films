8efb66404862e9dab9e02b5ae492f16e810eeda4
nestifff <nestiv.01.12@gmail.com>
2020-11-25 20:51:08 +0300

    fix/fix_rating_bar_and_nested_layout_issues

--- app/src/main/java/com/example/films/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/films/MovieDetailsActivity.kt
@@ -1,17 +1,35 @@
 package com.example.films
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.PorterDuff
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
+import android.widget.RatingBar
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MovieDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
 
-        try {
-            this.supportActionBar!!.hide()
-        } catch (e: NullPointerException) { }
+        this.supportActionBar?.hide()
 
+        /*val ratingBar = findViewById<RatingBar>(R.id.rating_bar)
+
+        val stars = ratingBar.progressDrawable as LayerDrawable
+        stars.getDrawable(2).setColorFilter(
+            resources.getColor(R.color.pink),
+            PorterDuff.Mode.SRC_ATOP
+        )
+        stars.getDrawable(1).setColorFilter(
+            resources.getColor(R.color.pink),
+            PorterDuff.Mode.SRC_ATOP
+        )
+        stars.getDrawable(0).setColorFilter(
+            resources.getColor(R.color.gray),
+            PorterDuff.Mode.SRC_ATOP
+        )*/
     }
 }
